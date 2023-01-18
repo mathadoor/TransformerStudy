520cd4d6bd53981322da6b558251e114fcec5a53
Harpreet Matharoo <harpreetm@cadmicro.com>
2023-01-18 15:24:47 -0500

    Updated the scope in readme

--- ReadMe.md
+++ ReadMe.md
@@ -3,8 +3,12 @@
 ## Introduction
 The key motivation behind this study is to analyze the learned representations of vision transformers  
 and convolutional neural networks. The end goal is still in its infancy and this repository serves as  
-a sandbox environment to that end. 
+a sandbox environment to develop that goal. 
 
 ## Plan
-The first task I will perform as a part of this study is to recreate some results from the seminal paper  
-[Do Vision Transformers see Like CNN?](https://arxiv.org/pdf/2108.08810.pdf)
+I will be performing the following tasks as a means to analyze the representations.
+0. Implement various similarity measures on various toy datasets as suggested by [Kornblith et. al](http://proceedings.mlr.press/v97/kornblith19a/kornblith19a.pdf).
+The idea is to reproduce the results by the authors.
+1. Then I will use the concepts learned from the above to recreate some results from the seminal paper 
+[Do Vision Transformers see Like CNN?](https://arxiv.org/pdf/2108.08810.pdf).
+2. In addition to the above, I should also see how the other similarity measures apply for analysis.
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from tqdm import tqdm
 import torchvision
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from torchvision.models import vit_l_16, ViT_L_16_Weights
+from torchvision.models import vit_b_32, ViT_B_32_Weights
 import numpy as np
 
 BATCH_SIZE = 32
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Import the ViT-b model
-model = vit_l_16(weights=ViT_L_16_Weights).to(DEVICE)
+model = vit_b_32(weights=vit_b_32.IMAGENET1K_V1).to(DEVICE)
 # Download the CIFAR10 dataset
 transform = transforms.Compose([transforms.Resize(224), transforms.ToTensor()])
 cifar10 = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
@@ -20,22 +20,22 @@ dataloader = DataLoader(cifar10, batch_size=BATCH_SIZE, shuffle=True)
 
 # Create a tensor to store the CKA values
 cka = torch.zeros((len(model.encoder.layers), len(model.encoder.layers))).to(DEVICE)
-
+features = []
 # Forward each batch through the model
 with torch.no_grad():
-  for i, (inputs, labels) in enumerate(tqdm(dataloader)):
-      # Extract the feature representations at each layer
-      x = model._process_input(inputs.to(DEVICE))
-      n = x.shape[0]
+    for i, (inputs, labels) in enumerate(tqdm(dataloader)):
+        # Extract the feature representations at each layer
+        x = model._process_input(inputs.to(DEVICE))
+        n = x.shape[0]
 
-      # Expand the class token to the full batch
-      batch_class_token = model.class_token.expand(n, -1, -1)
-      x = torch.cat([batch_class_token, x], dim=1)
-      # Extract the feature representations at each layer
-      features = model.encoder(x)
+        # Expand the class token to the full batch
+        batch_class_token = model.class_token.expand(n, -1, -1)
+        x = torch.cat([batch_class_token, x], dim=1)
 
-      for j in range(len(model.encoder.layers)):
-          for k in range(len(model.encoder.layers)):
-              cka[j, k] += (torch.mean(features[j] * features[k]) - torch.mean(features[j]) * torch.mean(features[k]))
+        # Extract the feature representations at each layer
+        features.append(model.encoder(x))
 
-cka /= len(dataloader)
+for j in range(len(model.encoder.layers)):
+    for k in range(len(model.encoder.layers)):
+        # cka[j, k] += (torch.mean(features[j] * features[k]) - torch.mean(features[j]) * torch.mean(features[k])
+        cka[j, k] = torch.trace(features[j]@features[k])
